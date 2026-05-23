88210ff54d0a79939c55697f839790b05128549a
RUNEX <artem.goose9009@gmail.com>
2026-05-23 14:37:14 +0300

    Update gg.lua

--- gg.lua
+++ gg.lua
@@ -1,9 +1,9 @@
 local KeySystem = loadstring(game:HttpGet("https://raw.githubusercontent.com/Vortex-scripts/BloxyBin-Key-System/main/main.lua"))()
 
 KeySystem.Initialize({
-    Script_Name = "Название твоего чита",  -- Название
-    Script_Creator = "Твой ник",          -- Твой авторский ник
-    Paste_ID = "ТВОЙ_PASTE_ID",           -- Сюда вставь ID из личного кабинета BloxyBin
+    Script_Name = "RNXhub",  -- Название
+    Script_Creator = "rnxhub",          -- Твой авторский ник
+    Paste_ID = "6767676767676767",           -- Сюда вставь ID из личного кабинета BloxyBin
     Callback = function(key)
         -- Здесь будет запускаться основной код чита после успешного ввода ключа
         print("Ключ принят! Запуск чита...")
